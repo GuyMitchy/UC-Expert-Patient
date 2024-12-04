0a0f22533c41eb61df22f798bd7438312345773d
Guy Clements <Guy.Clements123@googlemail.com>
2024-12-04 15:16:16 +0000

    Register admin models for food admin anel

--- foods/admin.py
+++ foods/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Food
 
-# Register your models here.
+@admin.register(Food)
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ('user', 'food_name', 'meal_type', 'date', 'is_trigger')
+    list_filter = ('meal_type', 'is_trigger', 'date')
+    search_fields = ('user__username', 'food_name', 'notes')
+    date_hierarchy = 'date'

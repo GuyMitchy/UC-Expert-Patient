c23925d83d2ef4db81fae3f938440822f7edfa2f
Guy Clements <Guy.Clements123@googlemail.com>
2024-12-07 13:34:18 +0000

    Add techsini to trusted orgigines for mockup

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,8 @@ ALLOWED_HOSTS = ['8000-guymitchy-ucexpertpatie-sv6c12l37dh.ws.codeinstitute-ide.
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeinstitute-ide.net/",
-    "https://*.herokuapp.com"
+    "https://*.herokuapp.com",
+    "https://techsini.com/multi-mockup/index.php",
 ]
 # Application definition
 
